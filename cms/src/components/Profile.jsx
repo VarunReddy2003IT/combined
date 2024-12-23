6cbc9ebf535fc17055925e821975420904dc6d8d
varun <varunreddypareddy@gmail.com>
2024-12-23 10:22:17 +0530

    profile update

---
+++ cms/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h2>profile Clubs</h2>
+      <p>Explore profile clubs and their activities.</p>
+    </div>
+  );
+}
+
+export default Profile;
