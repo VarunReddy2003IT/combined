14ab1ad55a89587f1224b1b6d0591bd6a2de1309
varun <varunreddypareddy@gmail.com>
2025-02-15 15:51:24 +0530

    added correct css for nav bar

--- cms/src/Navbar.jsx
+++ cms/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect,useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from './components/AuthContext';
 import './Navbar.css';
@@ -6,6 +6,7 @@ import './Navbar.css';
 function Navbar() {
   const { isLoggedIn, logout } = useContext(AuthContext);
   const [displayText, setDisplayText] = useState("Clubs"); 
+
   useEffect(() => {
     let currentText = "Clubs";
     const intervalId = setInterval(() => {
@@ -15,25 +16,28 @@ function Navbar() {
 
     return () => clearInterval(intervalId);
   }, []);
+
   return (
     <nav className="navbar">
-      <ul>
-        <li><Link to="/app">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        {isLoggedIn ? (
-          <>
-            <li><Link to="/clubs">{displayText}</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><button onClick={logout}>Logout</button></li> {/* Logout button styled like a link */}
-            <li><Link to="/notifications">Notification</Link></li>
-          </>
-        ) : (
-          <>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-          </>
-        )}
-      </ul>
+      <div className="navbar-wrapper">
+        <ul>
+          <li><Link to="/app">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          {isLoggedIn ? (
+            <>
+              <li><Link to="/clubs">{displayText}</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><button onClick={logout}>Logout</button></li> 
+              <li><Link to="/notifications">Notification</Link></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 }
